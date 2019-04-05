1333abbfb17fb3ad13681cf26fb78dfcdfcd87c8
Padraig  Gleeson <p.gleeson@gmail.com>
2019-04-05 17:51:35 -0400

    Work in progress...

--- tvb_lems/genlems.py
+++ tvb_lems/genlems.py
@@ -7,7 +7,7 @@
 """
 
 import lems.api as lems
-from . import source
+import source
 
 
 def model_instances():
@@ -24,12 +24,33 @@ def build_lems_for_model(src):
     # model.add(lems.Dimension('au'))
 
     # primary element of the model is a mass model component
-    mass = lems.ComponentType(src.name)
+    mass = lems.ComponentType(src.name, extends="baseCellMembPot")
     model.add(mass)
+    
+    ######### Adding v is required to ease mapping to NEURON...
+    mass.dynamics.add(lems.StateVariable(name="v", dimension="voltage", exposure="v"))
+    
+    mass.add(lems.Attachments(name="synapses",type_="basePointCurrentDL"))
+    
 
+    for input in src.input:
+        mass.dynamics.add(lems.DerivedVariable(name=input, 
+                                               dimension='none',
+                                               exposure=input,
+                                               select='synapses[*]/I',
+                                               reduce='add'))
+        mass.add(lems.Exposure(input, 'none'))
+        
+        
     for key, val in src.const.items():
         mass.add(lems.Parameter(key, 'none'))  # TODO units
+        
+    
+        
+    mass.add(lems.Constant(name="MSEC", dimension="time", value="1ms"))
 
+    states = []
+    der_vars = []
     # for key in src.param:
     #     mass.add(lems.Parameter(key, 'au'))  # TODO units
 
@@ -38,13 +59,25 @@ def build_lems_for_model(src):
         mass.dynamics.add(lems.DerivedVariable(key, value=val))
 
     for key in src.obsrv:
-        mass.add(lems.Exposure(key, 'none'))
+        name_dv = key.replace('(','_').replace(')','')
+        mass.dynamics.add(lems.DerivedVariable(name_dv, value=key, exposure=name_dv))
+        mass.add(lems.Exposure(name_dv, 'none'))
 
     for src_svar in src.state_space:
         name = src_svar.name
         ddt = src_svar.drift.replace('**', '^')
         mass.dynamics.add(lems.StateVariable(name, 'none', name))
-        mass.dynamics.add(lems.TimeDerivative(name, ddt))
+        mass.dynamics.add(lems.TimeDerivative(name, '(%s)/MSEC'%ddt))
+        mass.add(lems.Exposure(name, 'none'))
+        
+    ''' On condition is not need on the model but NeuroML requires its definition -->
+            <OnCondition test="r .lt. 0">
+                <EventOut port="spike"/>
+            </OnCondition>'''
+            
+    oc = lems.OnCondition(test='v .gt. 0')
+    oc.actions.append(lems.EventOut(port='spike'))
+    mass.dynamics.add(oc)
 
     return model
 
@@ -58,6 +91,7 @@ if __name__ == '__main__':
         fname = os.path.join(here, model.name + '.lems.xml')
         try:
             build_lems_for_model(model).export_to_file(fname)
+            print('Successfully built %s'%fname)
             validate_lems(fname)
         except Exception as exc:
-            print(exc)
+            print('> Error converting %s: %s'%(fname,exc))
